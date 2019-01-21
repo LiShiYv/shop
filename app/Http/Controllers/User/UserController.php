8b6899e3aff45162999e7a418d76f4209b66c7a5
LiShiYv <qq15354304250@163.com>
2019-01-21 09:21:48 +0800

    first commit

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Model\UserModel;
+
+class UserController extends Controller
+{
+    //
+
+	public function user($uid)
+	{
+		echo $uid;
+	}
+
+	public function test()
+    {
+        echo '<pre>';print_r($_GET);echo '</pre>';
+    }
+
+	public function add()
+	{
+		$data = [
+			'name'      => str_random(5),
+			'age'       => mt_rand(20,99),
+			'email'     => str_random(6) . '@gmail.com',
+			'reg_time'  => time()
+		];
+
+		$id = UserModel::insertGetId($data);
+		var_dump($id);
+	}
+}
